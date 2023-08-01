4680fae62bdd66b9afc1e8a696fc77da54bbb6a1
Miguel <miguelriostangarife@gmail.com>
2023-07-31 20:38:07 -0500

    Spring initializr de apimanagewood

---
+++ apimanagewood/src/test/java/com/uco/managewood/apimanagewood/ApimanagewoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uco.managewood.apimanagewood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApimanagewoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
