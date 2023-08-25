3d9cd218ba7bd362b2cebf9ac02323617494e68f
Miguel <miguelriostangarife@gmail.com>
2023-08-24 19:44:45 -0500

    AvanceClase24Agosto

--- src/main/java/com/uco/managewood/apimanagewood/domain/colaborador/Colaborador.java
+++ src/main/java/com/uco/managewood/apimanagewood/domain/colaborador/Colaborador.java
@@ -1,12 +1,45 @@
 package com.uco.managewood.apimanagewood.domain.colaborador;
 
 import com.uco.managewood.apimanagewood.domain.sede.Sede;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table( name = "colaborador")
+@Getter
+@Setter
 
 public class Colaborador {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+
+
     private int codigo;
+    @Column(name = "codigo")
+
+    private String nombre;
+    @Column(name = "nombre")
+    private int identificacion;
+    @Column(name="identificacion")
+
+
+    @ManyToOne
+    @JoinColumn(name = "codigoTipoColaborador", insertable = false,updatable = false)
     private TipoColaborador tipoColaborador;
+
+    @ManyToOne
+    @JoinColumn(name = "codigoEstadoColaborador", insertable = false,updatable = false)
     private EstadoColaborador estadoColaborador;
+
+    @ManyToOne
+    @JoinColumn(name = "codigoSede")
     private int codigoSede;
+
+
 
     public Colaborador(){
 
@@ -50,5 +83,21 @@ public class Colaborador {
 
     public void setSede(Sede sede) {
         this.codigoSede = sede.getCodigo();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(int identificacion) {
+        this.identificacion = identificacion;
     }
 }
