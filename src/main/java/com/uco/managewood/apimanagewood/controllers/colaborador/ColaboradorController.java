a56c06a36a278db1c1537ba7c30e5e45dcbdbd91
juanj01707 <90587297+juanj01707@users.noreply.github.com>
2023-09-10 15:03:28 -0500

    POLLO

--- src/main/java/com/uco/managewood/apimanagewood/controllers/colaborador/ColaboradorController.java
+++ src/main/java/com/uco/managewood/apimanagewood/controllers/colaborador/ColaboradorController.java
@@ -2,8 +2,8 @@ package com.uco.managewood.apimanagewood.controllers.colaborador;
 //RAMASS
 //RAMASS
 
-//RAMEN
-//SUSHI
+//CHICHARON
+//POLLO
 import com.uco.managewood.apimanagewood.domain.colaborador.Colaborador;
 import com.uco.managewood.apimanagewood.domain.sede.Sede;
 import com.uco.managewood.apimanagewood.service.colaborador.ColaboradorService;
