31341dcf009fcb9c109a05cf97d7fbc2a23833a6
juanj01707 <90587297+juanj01707@users.noreply.github.com>
2023-09-11 17:27:30 -0500

    Correcciones de sonar

--- src/main/java/com/uco/managewood/apimanagewood/validators/SedeValidator.java
+++ src/main/java/com/uco/managewood/apimanagewood/validators/SedeValidator.java
@@ -9,24 +9,23 @@ import org.springframework.validation.Validator;
 @Component
 public class SedeValidator implements Validator {
     private Errors errors;
+    private static final String FIELD_REQUIRED = "field.required";
 
     @Override
     public boolean supports(Class<?> clazz) {
         return Sede.class.equals(clazz);
     }
 
-
     @Override
     public void validate(Object target, Errors errors) {
         this.errors = errors;
         Sede sede = (Sede) target;
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nombre", "field.required", "El campo 'nombre' es obligatorio.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "codigoempresa", "field.required", "El campo 'codigoempresa' es obligatorio.");
-        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "codigociudad", "field.required", "El campo 'codigociudad' es obligatorio.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "nombre", FIELD_REQUIRED, "El campo 'nombre' es obligatorio.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "codigoempresa", FIELD_REQUIRED, "El campo 'codigoempresa' es obligatorio.");
+        ValidationUtils.rejectIfEmptyOrWhitespace(errors, "codigociudad", FIELD_REQUIRED, "El campo 'codigociudad' es obligatorio.");
     }
 
     public boolean hasErrors() {
         return errors != null && errors.hasErrors();
     }
-    
 }
