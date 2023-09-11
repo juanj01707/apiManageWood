2bb8b8e03a81829e95b24c55327abcd864ac4bac
Miguel <miguelriostangarife@gmail.com>
2023-09-10 22:09:40 -0500

    Buenas practicas en controllers

--- src/main/java/com/uco/managewood/apimanagewood/controllers/inventario/InventarioController.java
+++ src/main/java/com/uco/managewood/apimanagewood/controllers/inventario/InventarioController.java
@@ -20,7 +20,7 @@
         private InventarioService inventarioService;
 
 
-        @GetMapping(value = "/inventario")
+        @GetMapping(value = "/inventarios")
         public ResponseEntity<List<Inventario>> getAllInventarios(){
             List<Inventario> inventarios = inventarioService.findAll();
             if (!inventarios.isEmpty()) {
@@ -30,28 +30,28 @@
             }
         }
 
-        @GetMapping(value = "/inventario/{codigo}")
+        @GetMapping(value = "/inventarios/{codigo}")
         public ResponseEntity<Optional<Inventario>> findInventarioById(@PathVariable("codigo") Integer codigo){
             return ResponseEntity.status(HttpStatus.OK).body(inventarioService.findById(codigo));
         }
 
-        @PostMapping(value = "/inventario")
+        @PostMapping(value = "/inventarios")
         public ResponseEntity<Inventario> saveInventario(@RequestBody Inventario inventario){
             return ResponseEntity.status(HttpStatus.CREATED).body(inventarioService.saveInventario(inventario));
         }
 
-        @DeleteMapping(value = "/inventario/{codigo}")
+        @DeleteMapping(value = "/inventarios/{codigo}")
         public void deleteInventario(@PathVariable("codigo") Integer codigo){
             inventarioService.deleteInventario(codigo);
         }
 
-        @PutMapping(value = "/inventario/{codigo}")
+        @PutMapping(value = "/inventarios/{codigo}")
         public ResponseEntity<Inventario> updateInventario(@PathVariable("codigo") Integer codigo, @RequestBody Inventario nuevoInventario) {
             Inventario inventarioActualizado = inventarioService.updateInventario(codigo, nuevoInventario);
             return ResponseEntity.status(HttpStatus.OK).body(inventarioActualizado);
         }
 
-        @PatchMapping(value = "/inventario/{codigo}")
+        @PatchMapping(value = "/inventarios/{codigo}")
         public Inventario patchInventario(@PathVariable("codigo") Integer codigo, @RequestBody Map<String, Object> fields){
             return inventarioService.patchInventario(codigo,fields);
         }
